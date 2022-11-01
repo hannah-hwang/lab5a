64a18a0e78b5b3966d0c0d81b0accc9f65dc8f3a
Hannah Hwang <hannah.hwang1125@gmail.com>
2022-10-31 23:27:01 -0700

    problem 2

--- js/app.js
+++ js/app.js
@@ -31,11 +31,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+    let twoProduct = a * b;
+    console.log('value of twoProduct: ' + twoProduct);
+    let string = "The product of " + a + " and " + b + " is " + twoProduct + ".";
+    console.log(string);
+    let result = [twoProduct, string];
+    return result;
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
